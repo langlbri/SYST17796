41a66117a527dfd96477c6f64a39f923177c7015
langlbri <langlbri@sheridancollege.ca>
2025-01-21 16:54:11 -0500

    master

---
+++ .gitignore.kt
@@ -0,0 +1,8 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Kotlin.kt to edit this template
+ */
+
+fun main(args: Array<String>) {
+  println("Hello World!")
+}
